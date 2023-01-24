1325acd892b6c43dc4a6f352652fe5cff7d28dc4
Josue Martinez <josue.mart@jmcsoluciones.com>
2023-01-23 19:44:11 -0600

    Se agregan los botones directos del menu en la versión desktop

---
+++ components/footer-staff.php
@@ -0,0 +1,18 @@
+
+<footer>
+  <div class="footer__box">
+    
+    <div class="footer__foot"> 
+      <div class="rights">
+        <p>FUNDACIÓN PINITA 2023</p>
+      </div>
+      <p class="divisor"><strong>|</strong></p>
+      <div class="link__jmc"> <a href="https://esaujosue.dev" target="_blank">Desarrollado por <strong>Esaú Josué Martínez Carrasco</strong>  </a></div>
+    </div>
+  </div>
+  <script src="../js/menu.js"></script>
+  <script src="../js/script.js"></script>
+
+</footer>
+  </body>
+</html>
--- indexUsr.php
+++ indexUsr.php
@@ -7,7 +7,7 @@ include 'components/head.php';
 include 'components/permisos-menu.php';
 ?>
  <div class="title__box">
-    <h2 class="title__box__title">Fundación Pinita</h2>
+    <h2 class="title__box__title">Sistema Web Fundación Pinita</h2>
     <div class="title__box__usr">
         <p class="title__box__usr-name"><strong>Usuario:</strong>  <?php  echo $_SESSION['nombreUsr'] ?></p>
         <p class="title__box__usr-perfil"><strong>Perfil:</strong> <?php  echo $_SESSION['perfilUsr'] ?></p>
@@ -15,13 +15,46 @@ include 'components/permisos-menu.php';
     </div>
 </div>
 <div class="content">
-   
- <img src="/images/desk.jpg" alt="">
+<section class="content__buttons">
+      <ul class="content__buttons__items">
+        <li class="content__buttons__items-item">
+          <a href="">
+            <img src="/images/Iconos/teamwork.png" alt="">Staff
+          </a>
+        </li>
+        <li class="content__buttons__items-item">
+          <a href="">
+            <img src="/images/Iconos/social.png" alt="">Programas Sociales
+          </a>
+        </li>
+        <li class="content__buttons__items-item">
+          <a href="">
+            <img src="/images/Iconos/agreement.png" alt="">Aceptación de apoyos
+          </a>
+        </li>
+        <li class="content__buttons__items-item">
+          <a href="">
+            <img src="/images/Iconos/report.png"" alt="">Reportes
+          </a>
+        </li>
+        <li class="content__buttons__items-item">
+          <a href="">
+            <img src="/images/Iconos/online-shop.png" alt="">Ecommerce
+          </a>
+        </li>
+        <li class="content__buttons__items-item">
+          <a href="">
+            <img src="/images/Iconos/group.png" alt="">Usuarios
+          </a>
+        </li>
+      </ul>
+    </section>
+   <img src="/images/desk.jpg" alt="" class="content__wallpaper">
 </div>
 
 <?php
 }else{
     echo "Error en el Sistema";
 }
-include './components/footer.php';
+include './components/footer-staff.php';
 ?>
